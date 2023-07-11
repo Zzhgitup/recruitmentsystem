14c12dfdc8e07d4770d812d0d221d9d93164862a
Zzhgitup <99540215+Zzhgitup@users.noreply.github.com>
2023-07-11 15:40:19 +0800

    ✨ feat:
    移动端自适应基础配置

--- src/views/studentApply/index.tsx
+++ src/views/studentApply/index.tsx
@@ -1,10 +1,15 @@
 import React, { memo } from 'react';
 import type { FC, ReactNode } from 'react';
+import { Continer } from './style';
+import './css/index.less';
 interface IProps {
   children?: ReactNode;
 }
-
 const StudentApply: FC<IProps> = () => {
-  return <div>我是StudentApply</div>;
+  return (
+    <Continer>
+      <button className="bt">加速</button>
+    </Continer>
+  );
 };
 export default memo(StudentApply);
